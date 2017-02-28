b4c9a9d310a8fae231182267fd2b976c2224ec8c
Travis <trvshrmn@gmail.com>
2017-02-28 21:30:55 +0530

    fixed markdown class case

--- config/app.php
+++ config/app.php
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'MarkDown' => App\Services\Markdown::class,
+        'Markdown' => App\Services\Markdown::class,
 
     ],
 
